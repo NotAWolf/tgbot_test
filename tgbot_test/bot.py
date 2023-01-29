fd60f94c11cd0c84793e2020870689932a8fe314
Valet Grakovskih <valik.grakovich@gmail.com>
2023-01-29 15:08:51 +0300

    feat: constant messages start and help

--- tgbot_test/bot.py
+++ tgbot_test/bot.py
@@ -2,6 +2,8 @@ import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import bot_messeges
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -21,12 +23,25 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
     await context.bot.send_message(
             chat_id=effective_chat.id,
-            text="I'm a bot, please talk to me!")
+            text=bot_messeges.GREETINGS)
+
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_chat is None")
+        return
+    await context.bot.send_message(
+            chat_id=effective_chat.id,
+            text=bot_messeges.HELP)
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
-    
+    help_handler = CommandHandler('help', help)
+    application.add_handler(help_handler)
+        
     application.run_polling()
